a99f226e3e5d997203978fa25745dfb79ae0a8af
Jan Migoń <jan.migon@consonance.tech>
2020-05-22 19:42:08 +0200

    Migrate

--- app.py
+++ app.py
@@ -108,10 +108,8 @@ def save():
                   selfimpr, job, status)
     db.session.add(fd)
     db.session.commit()
-
     return redirect('/results')
 
+
 if __name__ == "__main__":
-    #manager.run()
     app.run()
-    #results()
