5d626e535f0702458fa581a7e74b1df067929ab3
Jan Migoń <jan.migon@consonance.tech>
2020-05-22 16:04:40 +0200

    Add new database connection

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 #! /usr/bin/python
 import numpy as np
+import psycopg2
 
 from utils import results_utils as ru
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wyniki.db'
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
 
 
 class Formdata(db.Model):
